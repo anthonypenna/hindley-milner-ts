997eb27b99db636cb17647dc0425747d7748468a
Anthony Penna <anthony.penna@musement.com>
2021-03-31 17:11:46 +0200

    feat: emitter

---
+++ src/Emitter/index.ts
@@ -0,0 +1,28 @@
+import { FunctionSignatureAst, IdentifierAst, LambdaExpressionAst, ValueSignatureAst } from "@/Parser"
+
+export function emitIdentifier(identifier: IdentifierAst): string {
+  return identifier.text
+}
+
+export function emitLambdaExpression(lambdaExpression: LambdaExpressionAst): string {
+  const generics = lambdaExpression.generics.map(emitIdentifier).join(", ")
+  const parameters = lambdaExpression.parameters
+    .reduce((result, parameter, index) => `${result} (arg${index}: ${emitIdentifier(parameter)}) =>`, "")
+    .trim()
+  const result = emitIdentifier(lambdaExpression.result)
+  return lambdaExpression.generics.length ? `<${generics}>${parameters} ${result}` : `${parameters} ${result}`
+}
+
+export function emitValueSignature(valueSignature: ValueSignatureAst): string {
+  const name = emitIdentifier(valueSignature.name)
+  const value = emitIdentifier(valueSignature.value)
+  const capitalizedName = name.slice(0, 1).toUpperCase() + name.slice(1)
+  return `type ${capitalizedName} = ${value}`
+}
+
+export function emitFunctionSignature(functionSignature: FunctionSignatureAst): string {
+  const name = emitIdentifier(functionSignature.name)
+  const body = emitLambdaExpression(functionSignature.body)
+  const capitalizedName = name.slice(0, 1).toUpperCase() + name.slice(1)
+  return `type ${capitalizedName} = ${body}`
+}
